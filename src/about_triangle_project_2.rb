6c69bcb0ef2d682822d34841e87c3233324352e5
Jim Weirich <jim.weirich@gmail.com>
2010-01-15 09:39:48 -0500

    Added [1,1,3] case for triangles.

--- src/about_triangle_project_2.rb
+++ src/about_triangle_project_2.rb
@@ -9,6 +9,7 @@ class AboutTriangleAssignment2 < EdgeCase::Koan
   def test_illegal_triangles_throw_exceptions
     assert_raise(TriangleError) do triangle(0, 0, 0) end
     assert_raise(TriangleError) do triangle(3, 4, -5) end
+    assert_raise(TriangleError) do triangle(1, 1, 3) end
     assert_raise(TriangleError) do triangle(2, 4, 2) end 
  end
 end
