c0bbe773d9c396a6033e735719f97b9931bf4087
Jim Weirich <jim.weirich@gmail.com>
2010-09-28 14:15:08 -0400

    Moved splat example to later + several typo corrections.

--- src/about_array_assignment.rb
+++ src/about_array_assignment.rb
@@ -5,18 +5,12 @@ class AboutArrayAssignment < EdgeCase::Koan
     names = ["John", "Smith"]
     assert_equal __(["John", "Smith"]), names
   end
-  
+
   def test_parallel_assignments
     first_name, last_name = ["John", "Smith"]
     assert_equal __("John"), first_name
     assert_equal __("Smith"), last_name
   end
-
-   def test_parallel_assigments_with_splat_operator
-     first_name, *last_name = ["John", "Smith", "III"]
-     assert_equal "John", first_name
-     assert_equal ["Smith","III"], last_name
-   end
 
   def test_parallel_assignments_with_extra_values
     first_name, last_name = ["John", "Smith", "III"]
@@ -24,13 +18,19 @@ class AboutArrayAssignment < EdgeCase::Koan
     assert_equal __("Smith"), last_name
   end
 
-  def test_parallel_assignments_with_extra_variables
+  def test_parallel_assignments_with_splat_operator
+    first_name, *last_name = ["John", "Smith", "III"]
+    assert_equal "John", first_name
+    assert_equal ["Smith","III"], last_name
+  end
+
+  def test_parallel_assignments_with_too_few_variables
     first_name, last_name = ["Cher"]
     assert_equal __("Cher"), first_name
     assert_equal __(nil), last_name
   end
 
-  def test_parallel_assignements_with_subarrays
+  def test_parallel_assignments_with_subarrays
     first_name, last_name = [["Willie", "Rae"], "Johnson"]
     assert_equal __(["Willie", "Rae"]), first_name
     assert_equal __("Johnson"), last_name
