ccfa664b485cb3fa8c16e9f31204ea5b554f2016
Jim Weirich <jim.weirich@gmail.com>
2010-12-23 11:45:46 -0500

    [5553333] Updated java interop on to_java method.

--- koans/about_java_interop.rb
+++ koans/about_java_interop.rb
@@ -103,11 +103,9 @@ class AboutJavaInterop < EdgeCase::Koan
   end
 
   def test_some_ruby_objects_can_NOT_be_coerced_to_java
-    [[], {}, Object.new].each do |ruby_object|
-      assert_raise(___) do
-        ruby_object.to_java_class
-      end
-    end
+    assert_equal __, [].to_java.class == Java::JavaUtil::ArrayList
+    assert_equal __, {}.to_java.class == Java::JavaUtil::HashMap
+    assert_equal __, Object.new.to_java.class == Java::JavaLang::Object
   end
 
   def test_java_collections_are_enumerable
