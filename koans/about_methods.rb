0447d69a6d508368d8ee3ea46f484128c593b43b
Jim Weirich <jim.weirich@gmail.com>
2011-02-03 15:23:25 -0800

    my_same_class_method => my_method_in_the_same_class for clarity

--- koans/about_methods.rb
+++ koans/about_methods.rb
@@ -92,16 +92,16 @@ class AboutMethods < EdgeCase::Koan
 
   # ------------------------------------------------------------------
 
-  def my_same_class_method(a, b)
+  def my_method_in_the_same_class(a, b)
     a * b
   end
 
   def test_calling_methods_in_same_class
-    assert_equal __, my_same_class_method(3,4)
+    assert_equal __, my_method_in_the_same_class(3,4)
   end
 
   def test_calling_methods_in_same_class_with_explicit_receiver
-    assert_equal __, self.my_same_class_method(3,4)
+    assert_equal __, self.my_method_in_the_same_class(3,4)
   end
 
   # ------------------------------------------------------------------
