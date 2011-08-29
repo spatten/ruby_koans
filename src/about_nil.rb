0a4f89cae38d24a5e293c85e58c7f97dfa5f27be
Daemian Mack <daemianmack@gmail.com>
2011-08-29 17:34:35 -0300

    Edited src/about_nil.rb via GitHub

--- src/about_nil.rb
+++ src/about_nil.rb
@@ -8,7 +8,7 @@ class AboutNil < EdgeCase::Koan
   def test_you_dont_get_null_pointer_errors_when_calling_methods_on_nil
     # What happens when you call a method that doesn't exist.  The
     # following begin/rescue/end code block captures the exception and
-    # make some assertions about it.
+    # makes some assertions about it.
     begin
       nil.some_method_nil_doesnt_know_about
     rescue Exception => ex
