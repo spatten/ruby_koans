58238ef54a83b01ef2d5b9c41ebad754cb377b9e
Jim Weirich <jim.weirich@gmail.com>
2013-04-09 09:30:33 -0400

    Removed fixed object_id koan
    This koan is not as important now that Ruby 2 has changed all the
    object ids.

--- src/about_objects.rb
+++ src/about_objects.rb
@@ -30,12 +30,6 @@ class AboutObjects < EdgeCase::Koan
     assert_equal __(true), obj.object_id != another_obj.object_id
   end
 
-  def test_some_system_objects_always_have_the_same_id
-    assert_equal __(0), false.object_id
-    assert_equal __(2), true.object_id
-    assert_equal __(4), nil.object_id
-  end
-
   def test_small_integers_have_fixed_ids
     assert_equal __(1), 0.object_id
     assert_equal __(3), 1.object_id
