f4d9dc0db70766f2eff673024aeffa924d97fff2
Artur Moczulski <artur.moczulski@gmail.com>
2013-04-24 00:24:09 -0700

    Fixes a silly typo in the method name
    in method_with_keyword_arguments_with_mandatory_argument

--- src/about_keyword_arguments.rb
+++ src/about_keyword_arguments.rb
@@ -13,13 +13,13 @@ class AboutMethods < Neo::Koan
     assert_equal __([1, 2]), method_with_keyword_arguments(two: 2)
   end
 
-  def method_with_keywork_arguments_with_mandatory_argument(one, two: 2, three: 3)
+  def method_with_keyword_arguments_with_mandatory_argument(one, two: 2, three: 3)
     [one, two, three]
   end
 
   def test_keyword_arguments_with_wrong_number_of_arguments
     exception = assert_raise (___(ArgumentError)) do
-      method_with_keywork_arguments_with_mandatory_argument
+      method_with_keyword_arguments_with_mandatory_argument
     end
     assert_match(/#{__("wrong number of arguments")}/, exception.message)
   end
