aa614489b4763f5e4d1f4264f6fd5172f43f7b75
Tarik Merrlees <tarik.merrylees@madetech.com>
2022-02-25 15:39:10 +0000

    re-order first few tests to better match beginner's ability and progression

--- src/path_to_enlightenment.rb
+++ src/path_to_enlightenment.rb
@@ -3,21 +3,21 @@
 $LOAD_PATH << File.dirname(__FILE__)
 
 require 'about_asserts'
-require 'about_nil'
-require 'about_objects'
-require 'about_arrays'
-require 'about_array_assignment'
-require 'about_hashes'
+require 'about_true_and_false'
 require 'about_strings'
 require 'about_symbols'
-require 'about_regular_expressions'
+require 'about_arrays'
+require 'about_array_assignment'
+require 'about_objects'
+require 'about_nil'
+require 'about_hashes'
 require 'about_methods'
 in_ruby_version("2", "3") do
   require 'about_keyword_arguments'
 end
 require 'about_constants'
+require 'about_regular_expressions'
 require 'about_control_statements'
-require 'about_true_and_false'
 require 'about_triangle_project'
 require 'about_exceptions'
 require 'about_triangle_project_2'
